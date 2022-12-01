c838de127169ca819e19761929040a2bfb761f15
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2022-12-01 17:41:15 +0100

    Update init.sh

--- lectures/init.sh
+++ lectures/init.sh
@@ -9,7 +9,7 @@
 source /home/workspace/ros/devel/setup.bash
 
 # Uncomment this when your workspace is built
-# source /home/lectures/robot_programming_with_lisp/06_turtle_party/ros_ws/devel/setup.bash
+# echo "source /home/lectures/robot_programming_with_lisp/06_turtle_party/ros_ws/devel/setup.bash" >> ~/.bashrc
 # jupyter-lab --allow-root --no-browser --port 8888 --ip=0.0.0.0 &
 
 echo "Booting in headless mode, starting roscore."
